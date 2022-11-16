564b1a8017aa33c546f3528004c0f1b4ad5b9a8a
bulv0620 <1241278815@qq.com>
2022-11-16 23:09:33 +0800

    feat: update ui

--- config/constant.js
+++ config/constant.js
@@ -18,6 +18,7 @@ module.exports = {
         frame: false,
         focusable: true,
         resizable: false,
+        transparent: true,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false
